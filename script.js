304088b5d5f24abbc95ccbaa54bda60acd9c56aa
Oana Rotaru <oanarotaru03@gmail.com>
2026-08-30 17:08:12 +0300

    fix duplicate declaration and dark mode logic

--- script.js
+++ script.js
@@ -22,8 +22,19 @@ function showRandomQuote() {
 }
 
 button.addEventListener("click", showRandomQuote);
+
 const darkModeBtn = document.getElementById("darkModeBtn");
+
+if (localStorage.getItem("darkMode") === "on") {
+    document.body.classList.add("dark-mode");
+}
 
 darkModeBtn.addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "on");
+    } else {
+        localStorage.setItem("darkMode", "off");
+    }
 });
